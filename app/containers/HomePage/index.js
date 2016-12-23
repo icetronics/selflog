03ceb206197e33807917b77c67f567c8696565e8
icetronics <icetronics@gmail.com>
2016-12-23 16:20:52 +0100

    Initial commit based on react-boilerplate

---
+++ app/containers/HomePage/index.js
@@ -0,0 +1,9 @@
+import React from 'react'; 
+
+export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      
+    );
+  }
+}
